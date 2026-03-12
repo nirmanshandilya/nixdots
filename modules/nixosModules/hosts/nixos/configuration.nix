07a5eee0c8385db2b679497565b4e2f21b6b609f
rachitvrma <rachitve6h2g@users.noreply.github.com>
2026-03-12 22:10:47 +0530

    modify: fonts nixos module modified
    Looped `l` may not show due to italics not being picked

--- modules/nixosModules/hosts/nixos/configuration.nix
+++ modules/nixosModules/hosts/nixos/configuration.nix
@@ -1,4 +1,5 @@
-{ inputs, pkgs, ... }: {
+{ inputs, pkgs, ... }:
+{
   imports = [
     # 1. Hardware and Base
     ./hardware-configuration.nix
@@ -25,7 +26,10 @@
   boot.initrd.systemd.enable = true;
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelModules = ["i2c-dev" "i2c-piix4"];
+  boot.kernelModules = [
+    "i2c-dev"
+    "i2c-piix4"
+  ];
   boot.kernelParams = [
     "acpi_enforce_rsources=lax"
   ];
@@ -40,45 +44,92 @@
   services.tumbler.enable = true;
 
   # --- SYSTEM PACKAGES ---
-  # These are available to all users. 
+  # These are available to all users.
   # We moved Zsh and Niri specifics out, keeping general tools here.
   environment.systemPackages = with pkgs; [
     brightnessctl # Screen brightness control
-    playerctl     # Media (play/pause) control
-    git           # Version control
-    blueman       # Bluetooth manager GUI
-    libmtp        # Phone connection support
-    powertop      # Battery usage analysis
-    i2c-tools     # Hardware debugging
-    usbutils      # lsusb command
-    libnotify     # notify-send
+    playerctl # Media (play/pause) control
+    git # Version control
+    blueman # Bluetooth manager GUI
+    libmtp # Phone connection support
+    powertop # Battery usage analysis
+    i2c-tools # Hardware debugging
+    usbutils # lsusb command
+    libnotify # notify-send
   ];
 
   # --- auto mount HDD (/dev/sda1) at /mnt ---
   fileSystems."/mnt" = {
     device = "/dev/disk/by-uuid/4CF809A7F809907E";
     fsType = "ntfs3";
-    options = [ "rw" "uid=1000" "gid=100" "nofail" ];
+    options = [
+      "rw"
+      "uid=1000"
+      "gid=100"
+      "nofail"
+    ];
   };
 
   # --- BATTERY CONSERVATION MODE TOGGLE ---
   security.sudo.extraRules = [
-  {
-    users = [ "jawknee" ];
-    commands = [
-      { 
-        command = "/run/current-system/sw/bin/tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004\\:00/conservation_mode"; 
-        options = [ "NOPASSWD" ]; 
-      }
-    ];
-  }
-];
+    {
+      users = [ "jawknee" ];
+      commands = [
+        {
+          command = "/run/current-system/sw/bin/tee /sys/bus/platform/drivers/ideapad_acpi/VPC2004\\:00/conservation_mode";
+          options = [ "NOPASSWD" ];
+        }
+      ];
+    }
+  ];
 
   # --- FONTS ---
-  fonts.packages = with pkgs; [
-    #   nerd-fonts.jetbrains-mono
-    maple-mono.NF
-  ];
+  fonts = {
+    # Preferable to set to false, but maybe, I might try setting
+    # it to true and see what happens
+    enableDefaultPackages = false;
+    fontDir.enable = true;
+    packages = with pkgs; [
+      # Google Noto Fonts for normal day to day tasks
+      noto-fonts
+      noto-fonts-cjk-sans
+      noto-fonts-color-emoji
+
+      # Nerd Fonts
+      maple-mono.NF # NOTE: looped l may not show due to Italics not being used
+      nerd-fonts.symbols-only
+
+      # For document-like fonts
+      inter
+    ];
+
+    fontconfig = {
+      antialias = true;
+      hinting = {
+        enable = true;
+        style = "slight";
+      };
+      subpixel = {
+        rgba = "rgb";
+      };
+
+      defaultFonts = {
+        serif = [
+          "Noto Serif"
+        ];
+        sansSerif = [
+          "Inter"
+          "Noto Sans"
+          "Noto Sans CJK"
+        ];
+        monospace = [
+          "Maple Mono NF"
+          "Noto Sans Mono"
+        ];
+        emoji = [ "Noto Color Emoji" ];
+      };
+    };
+  };
 
   # --- PROGRAMS & SECURITY ---
   # Thunar: File manager with plugins for archives and volumes.
@@ -86,12 +137,12 @@
     enable = true;
     plugins = with pkgs.xfce; [
       thunar-volman
-      thunar-archive-plugin 
+      thunar-archive-plugin
     ];
   };
 
   # --- PAM Services for Swaylock ---
-  security.pam.services.swaylock = {};
+  security.pam.services.swaylock = { };
 
   # Enable sound with pipewire.
   security.rtkit.enable = true;
@@ -118,15 +169,15 @@
     allowedUDPPorts = [ 53317 ];
   };
 
-  environment.pathsToLink = [ 
-    "/share/xdg-desktop-portal" 
-    "/share/applications" 
+  environment.pathsToLink = [
+    "/share/xdg-desktop-portal"
+    "/share/applications"
   ];
 
   xdg.portal = {
-  enable = true;
-  extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  config.common.default = "gtk";
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    config.common.default = "gtk";
   };
 
   # --- HOME MANAGER ---
@@ -140,5 +191,5 @@
 
   # --- VERSIONING ---
   # This must stay the same as when you first installed.
-  system.stateVersion = "25.11"; 
+  system.stateVersion = "25.11";
 }
